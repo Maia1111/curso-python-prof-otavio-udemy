ec1d9eba8b468a10f12b2db9bbf7e63c24c17e3c
Rogerio Maia <rogerioreservadosinop@gmail.com>
2022-04-18 19:48:43 -0400

    Aula enumerate_2

---
+++ enumerate.py
@@ -0,0 +1,29 @@
+"""
+Enumerate - Enumerar elementos em uma lista # list
+
+"""
+
+lista = [
+    ['Edu', 'João', 'Luiz'],
+    ['Maria', 'Aline', 'Joana'],
+    ['Helena', 'Edu', 'Lu']
+]
+
+enumerada = enumerate(lista)
+print(enumerada)
+
+
+enumerada = list(enumerada)  # Cast do objeto enumate para list
+print(enumerada)
+
+# for em enumerate de lista
+for n1, n2 in enumerate(lista):
+    print(n1, n2)
+
+
+# Enumerate Não serve  exatamente pra controlar indice e sim pra enumerar
+# Você pode mudar o início do enumerate passando um parametro de início
+# Por padrão ele inicia em 0
+for n1, n2 in enumerate(lista, 53):
+    print(n1, n2)
+
