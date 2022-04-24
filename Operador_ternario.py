94b7e47801ac308427e1ab113c3b17866cd2a309
Rogerio Maia <rogerioreservadosinop@gmail.com>
2022-04-24 19:33:11 -0400

    Refatorado Operador_ternario

--- Operador_ternario.py
+++ Operador_ternario.py
@@ -8,7 +8,7 @@ logged_user = True
 msg = 'Usuário Logado!' if logged_user else 'Usuário precisa logar'
 print(msg)
 
-idade = input()
+idade = int(input('Digite sua idade: '))
 msg2 = 'Pode acessar' if idade >= 18 else 'Não pode acessar'
 
 print(msg2)
